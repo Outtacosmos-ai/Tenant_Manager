08cfa71aa28dde68f6d09404d45a1051b73734ba
Outtacosmos-ai <messghir4@gmail.com>
2025-01-10 19:51:47 +0100

    Fix AppConfig for appointments app.

--- dentiapro/backend/apps/appointments/apps.py
+++ dentiapro/backend/apps/appointments/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class AppointmentsConfig(AppConfig):
+class AuthenticationConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.appointments'
+    name = 'apps.authentication'
