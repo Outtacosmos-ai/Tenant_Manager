680d3be679eee50cca72792c88ff230705ea2d53
Outtacosmos-ai <messghir4@gmail.com>
2025-01-15 15:25:59 +0100

    Update WSGI configuration for environment setup

--- dentiapro/backend/config/wsgi.py
+++ dentiapro/backend/config/wsgi.py
@@ -1,16 +1,21 @@
 """
-WSGI config for config project.
+WSGI config for the Dentiapro project.
 
-It exposes the WSGI callable as a module-level variable named ``application``.
+This file exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+# Add the project directory to the Python path for module resolution
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
+# Set the default settings module for the 'django' program.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.production')
+
+# Create the WSGI application object
 application = get_wsgi_application()
